b26f9e175106b7b4efffeca40d042df2f343507b
kwassef1 <karimwassef4@gmail.com>
2026-03-17 14:43:44 -0400

    Added card component and form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
+import Card from "@/components/card";
 import Header from "@/components/header";
+import RentalForm from "@/components/rentalform";
+
 export default function Home() {
     return (
-        <div className="min-h-screen">
+        <div className="min-h-screen bg-gray-50">
             <Header />
-            <h1 className="text-4xl font-bold text-center mt-16">
-                Begory Capital
-            </h1>
+            <Card>
+                <RentalForm />
+            </Card>
+            {/* <div className="max-w-6xl mx-auto flex items-start justify-end py-12 px-4">
+                <div className="w-full max-w-lg">
+                    <div className="bg-white rounded-xl shadow-md p-6">
+                        <RentalForm />
+                    </div>
+                </div>
+            </div> */}
         </div>
     );
 }
---
+++ src/components/card.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+type CardProps = {
+    children: React.ReactNode;
+    className?: string;
+    title?: string;
+    tag?: string;
+};
+export default function Card({
+    children,
+    className = "",
+    title,
+    tag,
+}: CardProps) {
+    return (
+        <div className="w-full flex justify-center">
+            <div
+                className={`max-w-lg w-full flex-none bg-white rounded-2xl border border-gray-200 shadow-md p-6 ${className}`}
+            >
+                {title && <div className="card-title">{title}</div>}
+                {tag && (
+                    <span className="inline-block bg-blue-100 text-blue-800 text-xs px-2 py-1 rounded-full mb-2">
+                        {tag}
+                    </span>
+                )}
+                {children}
+            </div>
+        </div>
+    );
+}
